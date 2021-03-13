2136ff083d4b6ce28f6b50e8abdc06316699cf92
mbmminer <mbmminer@gmail.com>
2021-03-13 10:38:26 -0800

    Got to section 2.2
    Starting section 2.2, we have a plane and coordinates.

---
+++ HexMap Tutorial/Assets/Scripts/HexMetrics.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class HexMetrics : MonoBehaviour {
+    public const float outerRadius = 10f;
+
+	public const float innerRadius = outerRadius * 0.866025404f;
+
+    public static Vector3[] corners = {
+		new Vector3(0f, 0f, outerRadius),
+		new Vector3(innerRadius, 0f, 0.5f * outerRadius),
+		new Vector3(innerRadius, 0f, -0.5f * outerRadius),
+		new Vector3(0f, 0f, -outerRadius),
+		new Vector3(-innerRadius, 0f, -0.5f * outerRadius),
+		new Vector3(-innerRadius, 0f, 0.5f * outerRadius)
+	};
+}
