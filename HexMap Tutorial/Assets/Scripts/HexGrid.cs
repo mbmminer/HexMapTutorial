0f5a0ad44ce072941c59f67220ad2f335e5f04fb
mbmminer <mbmminer@gmail.com>
2021-03-13 11:01:04 -0800

    Right before section 3     Working at adding hexagons next

--- HexMap Tutorial/Assets/Scripts/HexGrid.cs
+++ HexMap Tutorial/Assets/Scripts/HexGrid.cs
@@ -26,9 +26,9 @@ public class HexGrid : MonoBehaviour {
 	
 	void CreateCell (int x, int z, int i) {
 		Vector3 position;
-		position.x = x * 10f;
+		position.x = (x + z * 0.5f - z / 2) * (HexMetrics.innerRadius * 2f);
 		position.y = 0f;
-		position.z = z * 10f;
+		position.z = z * (HexMetrics.outerRadius * 1.5f);
 
 		HexCell cell = cells[i] = Instantiate<HexCell>(cellPrefab);
 		cell.transform.SetParent(transform, false);
